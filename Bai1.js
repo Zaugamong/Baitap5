5a0fdf80841778ed50c0172f38cb560cb48ff8d9
Zaugamong <thanhsonnguyen2003@gmail.com>
2022-12-03 14:17:37 +0700

    Baitap5

---
+++ Bai1.js
@@ -0,0 +1,60 @@
+//Bai1
+// let a = ['Nguyen','Thanh','Son']
+// let b = ['1','2','3']
+// function gop(x) {
+//     x = console.log(a.concat(b));
+//     return x;
+// }
+// gop()
+
+//Bai2
+// let array = [2,4,6,8];
+// function test(a) {
+//     return a % 2 == 0
+// }
+// let sum = array.every(test,0);
+// console.log(sum);
+
+//Bai3
+// let array = [1,3,5,8];
+// function test(a) {
+//     return a % 2 == 0
+// }
+// let sum = array.some(test,0);
+// console.log(sum);
+
+//Bai4
+// let array = [1,3,5,8,0,-1];
+// function test(a) {
+//     return a > 0
+// }
+// let sum = array.filter(test,0);
+// console.log(sum);
+
+// Bai5
+// let array = [-1,-2,3,4,-5];
+// function test(a) {
+//     return a > 0;
+// }
+// let num1 = array.findIndex(test,0);
+// let num2 = array.find(test,0);
+// console.log(num1,num2);
+
+//Bai 6
+// let array = [1,5,30,26];
+// array.forEach(element => console.log(element));
+
+//Bai7
+
+//Bai8
+// let a = ['Nguyen', 'Thanh', 'Son'];
+// console.log(a.join());
+
+// Bai9
+// let array = [2,-1,-8];
+// array = parseInt;
+// function test(a) {
+//     a = Math.abs(array);
+//     return a
+// }
+// test()
